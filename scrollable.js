ec2500f0524a7ae304d9f4f843d6874c6174e2b3
Egge <80262989+Egge7@users.noreply.github.com>
2023-05-30 11:54:11 +0200

    add docs

--- scrollable.js
+++ scrollable.js
@@ -1,14 +1,25 @@
+/**
+ * Creates a scrollable ad slot and adds it to an HTMLelement on the page
+ * @param {HTMLElement} element - The element to inject the scrollable container into
+ * @param {string} adSlotId - The ID that will be assigned to the ad slot within the scrollable container.
+ */
 export function setupScrollable(element, adSlotId) {
+    // create variable to track active aniamtion
     let ticking = false;
+
+    // create container as wrapper for scrolling ad
     const scrollableContainer = document.createElement("div");
     scrollableContainer.style.height = "100%";
     scrollableContainer.style.width = "100%";
     scrollableContainer.style.overflow = "hidden";
 
+    // create container for creative and assign passed ID
     const adSlot = document.createElement("div");
     adSlot.id = adSlotId;
     adSlot.style.transition = "transform 5ms";
     scrollableContainer.appendChild(adSlot);
+
+    // add onScroll callback on intersection if Intersection Observer API is available
     if (IntersectionObserver) {
         const intersectionObserver = new IntersectionObserver(
             handleIntersection,
